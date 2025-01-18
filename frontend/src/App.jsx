7e920b2d1f66d694d2a618b1844b22da88fafb80
KannehaLakshmi <pji.kanneha.lakshmi@kalvium.community>
2025-01-18 14:15:30 +0530

    milestone6

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {LoginPage,SignupPage} from './Routes.jsx'
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      
+
+    </Routes>
+    </BrowserRouter>
+ 
+  );
+}
+
+export default App;
+
+
---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,7 @@
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
+export {LoginPage,SignupPage};
+
+
+
+
